df5aed76e6588eddda564a242a7281e1ab96e8a3
Yeasin <ar9485727@gmail.com>
2025-08-10 11:20:14 +0600

    lenis added

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,42 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './Home'
+import { useRef, useEffect } from 'react'
+import Lenis from 'lenis'
+import 'lenis/dist/lenis.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <GlobalLenisWrapper >
+      <Home />
+    </GlobalLenisWrapper>
   </StrictMode>,
 )
+
+
+function GlobalLenisWrapper({ children }) {
+  const lenis = useRef(null)
+
+  useEffect(() => {
+    // Initialize Lenis
+    lenis.current = new Lenis({
+      duration: 1.2,
+      easing: (t) => 1 - Math.pow(1 - t, 3),
+      smooth: true,
+      smoothTouch: true,
+    })
+
+    // RAF Loop
+    function raf(time) {
+      lenis.current.raf(time)
+      requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+
+    return () => {
+      lenis.current.destroy()
+    }
+  }, [])
+
+  return children
+}
